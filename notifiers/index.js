7a1b625d7810b78fa92cf4b029cea8ec1eda417f
tehpsalmist <bstewardcodes@gmail.com>
2018-12-20 03:29:46 -0500

    refactor: remove unused channels and namespaced sockets

--- notifiers/index.js
+++ notifiers/index.js
@@ -5,29 +5,53 @@ const { channels, Channel } = require('../channels')
 const port = process.env.SOCKET_PORT || 7000
 
 io.on('connect', client => {
-  client.on('channelRequest', (channelId, reply) => {
-    const namespace = requestChannel(channelId).on('connect', nspClient => {
-      channels[channelId].events.on('status', videoId => {
-        namespace.emit('status', { channelId, videoId })
-      })
-
-      channels[channelId].refreshStatus()
-    })
-
-    console.log('namespace:', namespace.name)
+  client.on('channelRequest', async (channelId, reply) => {
+    const namespace = await requestChannel(channelId)
 
     reply({ success: true, namespace: namespace.name })
   })
 })
 
+/**
+ * requests a namespaced socket, and creates a new Channel if that channel ID isn't active
+ * @param {string} id channel ID
+ *
+ * @returns {Promise<SocketIO.Namespace>}
+ */
 function requestChannel (id) {
-  if (channels[id] instanceof Channel) return io.of(`/${id}`)
+  const channelNameSpace = io.of(`/${id}`)
+
+  if (channels[id] instanceof Channel) return Promise.resolve(channelNameSpace)
 
   channels[id] = new Channel(id)
 
-  channels[id].subscribe()
+  channelNameSpace.on('connect', nspClient => {
+    channels[id].refreshStatus()
 
-  return io.of(`/${id}`)
+    nspClient.on('disconnect', message => {
+      console.log('message', message)
+      message === 'transport close' && channelNameSpace.clients((err, clients) => {
+        if (err) throw err
+
+        if (!clients.length) {
+          channels[id].unsubscribe().then(() => {
+            Object.keys(channelNameSpace.connected).forEach(key => {
+              channelNameSpace.connected[key].disconnect(true)
+            })
+            delete io.nsps[channelNameSpace.name]
+          })
+        }
+      })
+    })
+  })
+
+  return channels[id].subscribe().then(() => {
+    channels[id].events.on('status', videoId => {
+      channelNameSpace.emit('status', { channelId: id, videoId })
+    })
+
+    return channelNameSpace
+  })
 }
 
 module.exports = () => io.listen(port)
